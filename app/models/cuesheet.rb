b56b6b34ca01110a7061661d14ae30b5b67f595a
Blake Smith <blakesmith0@gmail.com>
2009-07-08 00:11:15 -0500

    Add some model validators

--- app/models/cuesheet.rb
+++ app/models/cuesheet.rb
@@ -1,3 +1,5 @@
 class Cuesheet < ActiveRecord::Base
   has_many :tracks
+
+  validates_presence_of :performer, :title, :file
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :cuesheet
+
+  validates_presence_of :performer, :title
+  validates_numericality_of :minutes, :seconds, :frames
 end
