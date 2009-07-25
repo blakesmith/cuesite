904c470a820270dc284eea30e5d8af508a9b5fdd
Blake Smith <blakesmith0@gmail.com>
2009-07-25 00:42:11 -0500

    Merge regexp functions into one method

--- test/unit/cuesheet_test.rb
+++ test/unit/cuesheet_test.rb
@@ -15,51 +15,30 @@ class CuesheetTest < ActiveSupport::TestCase
     assert cuesheet
   end
 
-  should 'load from file' do
-    cue = Cuesheet.new
-    assert_nothing_raised do
-      cue.load_from_file('test/fixtures/test.cue')
-    end
-  end
-
   context 'load from file' do
+
     setup do
-      @cue = Cuesheet.new
-      @cue.load_from_file('test/fixtures/test.cue')
+      @cue = create_cuesheet
+      @sheet = @cue.parse_cue_file('test/fixtures/test.cue')
+    end
+    
+    should 'return list of hashes for cuesheet data' do
+      test_hash = {:performer => "Essential Mix", :title => "Intro", :index => [0, 0, 0], :track => 1}
+      test_hash2 = {:performer => "Marc Houle", :title => "Meatier Shower", :index => [1, 41, 13], :track => 2}
+      assert_equal test_hash, @sheet[0]
+      assert_equal test_hash2, @sheet[1]
     end
 
-    should 'regex match track number' do
-      @cue.parse_track_numbers
+    should 'validate regexp' do
       assert_equal 35, @cue.track_numbers.size
-    end
-
-    should 'regex match track index' do
-      @cue.parse_indices
       assert_equal 35, @cue.indices.size
-    end
-
-    should 'regex match performer' do
-      @cue.parse_performers
       assert_equal 35, @cue.performers.size
-      assert_equal "Steve Mac", @cue.cue_performer
-    end
-
-    should 'regex match titles' do
-      @cue.parse_titles
       assert_equal 35, @cue.titles.size
+
+      assert_equal "Steve Mac", @cue.cue_performer
       assert_equal "Essential Mix (2008-10-25) [TMB]", @cue.cue_title
     end
 
   end
-    
-  should 'return list of hashes for cuesheet data' do
-    @cue = create_cuesheet
-    sheet = @cue.parse_cue_file('test/fixtures/test.cue')
-    test_hash = {:performer => "Essential Mix", :title => "Intro", :index => [0, 0, 0], :track => 1}
-    test_hash2 = {:performer => "Marc Houle", :title => "Meatier Shower", :index => [1, 41, 13], :track => 2}
-    assert_equal test_hash, sheet[0]
-    assert_equal test_hash2, sheet[1]
-  end
-
 
 end
