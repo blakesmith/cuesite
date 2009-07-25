9d6033ed26e92505fa57bc990b25f5b0cc610776
Blake Smith <blakesmith0@gmail.com>
2009-07-25 16:29:41 -0400

    Refactor parse_from_file to be a static method, with different hash structure

--- app/models/cuesheet.rb
+++ app/models/cuesheet.rb
@@ -5,28 +5,40 @@ class Cuesheet < ActiveRecord::Base
 
   attr_accessor :file, :indices, :performers, :cue_performer, :cue_title, :titles, :track_numbers
 
-  def parse_cue_file(file)
-    @file = File.open(file).read
-    @indices = @file.scan(/INDEX \d{1,2} (\d{1,3}):(\d{1,2}):(\d{1,2})/).collect do |index|
+  def self.load_from_file(file)
+    parsed = parse_cue_file(file)
+    cue = Cuesheet.create(:cue_performer => parsed[:cue_performer], :cue_title => parsed[:cue_title])
+    parsed.each do |track|
+      song = Song.find_or_create_by_performer_and_title_and_remix(:performer => track[:performer], :title => track[:title], :remix => track[:remix])
+      track = Track.create(:cuesheet => cue, :song => song, :minutes => parsed[:minutes], :seconds => parsed[:seconds], :frames => parsed[:frames])
+    end
+  end
+
+  def self.parse_cue_file(file)
+    f = File.open(file).read
+
+    performers = f.scan(/PERFORMER \"(.*)\"/).collect {|performer| performer[0]}
+    cue_performer = performers[0]
+    performers.delete_at(0)
+
+    titles = f.scan(/TITLE \"(.*)\"/).collect {|title| title[0]}
+    cue_title = titles[0]
+    titles.delete_at(0)
+
+    indices = f.scan(/INDEX \d{1,2} (\d{1,3}):(\d{1,2}):(\d{1,2})/).collect do |index|
       index.collect do |values|
         values.to_i
       end
     end
 
-    @performers = @file.scan(/PERFORMER \"(.*)\"/).collect {|performer| performer[0]}
-    @cue_performer = @performers[0]
-    @performers.delete_at(0)
+    track_numbers = f.scan(/TRACK (\d{1,3}) AUDIO/).collect {|track| track[0].to_i}
 
-    @titles = @file.scan(/TITLE \"(.*)\"/).collect {|title| title[0]}
-    @cue_title = @titles[0]
-    @titles.delete_at(0)
-
-    @track_numbers = @file.scan(/TRACK (\d{1,3}) AUDIO/).collect {|track| track[0].to_i}
-
-    parsed = []
-    (0..@track_numbers.size).each do |i|
-      parsed << {:performer => @performers[i], :title => @titles[i], :index => @indices[i], :track => @track_numbers[i]}
+    parsed = {:cuesheet => {:performer => cue_performer, :title => cue_title}, :tracks => []}
+    tracks = []
+    (0..track_numbers.size - 1).each do |i|
+      tracks << {:performer => performers[i], :title => titles[i], :index => indices[i], :track => track_numbers[i]}
     end 
+    parsed[:tracks] = tracks
     parsed
   end
 
--- test/unit/cuesheet_test.rb
+++ test/unit/cuesheet_test.rb
@@ -18,27 +18,38 @@ class CuesheetTest < ActiveSupport::TestCase
   context 'load from file' do
 
     setup do
-      @cue = create_cuesheet
-      @sheet = @cue.parse_cue_file('test/fixtures/test.cue')
+      @sheet = Cuesheet.parse_cue_file('test/fixtures/test.cue')
+    end
+
+    should 'return overall performer and title' do
+      test_hash = {:performer => 'Steve Mac', :title => 'Essential Mix (2008-10-25) [TMB]'}
+      assert_equal(test_hash, @sheet[:cuesheet])
     end
     
     should 'return list of hashes for cuesheet data' do
       test_hash = {:performer => "Essential Mix", :title => "Intro", :index => [0, 0, 0], :track => 1}
       test_hash2 = {:performer => "Marc Houle", :title => "Meatier Shower", :index => [1, 41, 13], :track => 2}
-      assert_equal test_hash, @sheet[0]
-      assert_equal test_hash2, @sheet[1]
+      assert_equal test_hash, @sheet[:tracks][0]
+      assert_equal test_hash2, @sheet[:tracks][1]
     end
 
-    should 'validate regexp' do
-      assert_equal 35, @cue.track_numbers.size
-      assert_equal 35, @cue.indices.size
-      assert_equal 35, @cue.performers.size
-      assert_equal 35, @cue.titles.size
-
-      assert_equal "Steve Mac", @cue.cue_performer
-      assert_equal "Essential Mix (2008-10-25) [TMB]", @cue.cue_title
+    should 'validate regexp tracks' do
+      assert_equal 35, @sheet[:tracks].size
+      @sheet[:tracks].each do |track|
+        assert_equal(4, track.size)
+      end
+      assert_equal('Surkin', @sheet[:tracks][34][:performer])
     end
 
-  end
+ end
 
+#  should 'load cuesheet into database' do
+#    Cuesheet.load_from_file('test/fixtures/test.cue')
+#    @cuesheet = find_by_performer('Steve Mac')
+#    assert @cuesheet
+#    assert_equal('Steve Mac', @cuesheet.performer)
+#    assert_equal('Intro', @cuesheet.songs[0])
+#    assert_equal(0, @cuesheet.tracks[0].minutes)
+#  end
+#
 end
