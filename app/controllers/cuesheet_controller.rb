8e6ee46b8df50f150fb0ef19c75cb74801c8cefb
Blake Smith <blakesmith0@gmail.com>
2009-08-08 18:02:35 -0400

    Destroy a cuesheet

--- app/controllers/cuesheet_controller.rb
+++ app/controllers/cuesheet_controller.rb
@@ -1,6 +1,11 @@
 class CuesheetController < ApplicationController
 
   def index
+    list
+    render :action => 'list'
+  end
+
+  def list
     @cuesheets = Cuesheet.all
   end
 
@@ -24,6 +29,16 @@ class CuesheetController < ApplicationController
     if params[:id]
       @cuesheet = Cuesheet.find(params[:id])
     end
+  end
+
+  def destroy
+    cuesheet = Cuesheet.find(params[:id])
+    if cuesheet.tracks.destroy_all && cuesheet.destroy
+      flash[:notice] = 'Cuesheet successfully removed!'
+    else
+      flash[:notice] = 'Cuesheet failed to remove.'
+    end
+    redirect_to :action => 'index'
   end
 
 end
