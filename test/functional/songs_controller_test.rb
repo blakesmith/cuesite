f24932ff72e3d57fe764784dd48fbc63c0a2e4ac
Blake Smith <blakesmith0@gmail.com>
2009-08-16 20:52:51 -0400

    Fix songs functional test

--- test/functional/songs_controller_test.rb
+++ test/functional/songs_controller_test.rb
@@ -11,7 +11,8 @@ class SongsControllerTest < ActionController::TestCase
 
   should 'PUT update' do
     song = create_song
-    xhr :put, :update, :id => song.id, :song => song
+    track = create_track :song => song
+    xhr :put, :update, :id => song.id, :song => song, :track => track.id
     assert_response :success
     assert_select_rjs :hide, "edit_song_#{song.id}"
     assert_select_rjs :show, "display_song_#{song.id}"
